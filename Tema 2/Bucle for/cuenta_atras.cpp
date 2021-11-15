cf353e789b6161dd0132f9b97f19e94e6639c3b4
Jose Maria <jose.maria.castelo@gmail.com>
2021-11-15 12:56:32 +0100

    Agregado más código tema 2

---
+++ Tema 2/Bucle for/cuenta_atras.cpp
@@ -0,0 +1,16 @@
+// Calculo Numerico: Tema 2 / Bucle for / cuenta_atras.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Bucle for que realiza la cuenta atras
+    for (int n = 10; n > 0; n--)
+        cout << n << ", ";
+
+    cout << "despegue!" << endl;
+
+    return 0;
+}
